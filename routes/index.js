ada68a759a87249426908b9cd98e6a463de37896
Vinayak Iyer <vinayak.iyer@theflyy.com>
2023-05-17 19:55:33 +0530

    Changed docker-compose to have live-reload

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({message: 'alive'});
+  res.json({message: 'alive ttdxd'});
 });
 
 module.exports = router;
